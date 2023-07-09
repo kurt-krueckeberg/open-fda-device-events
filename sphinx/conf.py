302b3e4e31d3ae07892831cf7bade1abc068457f
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-07-09 17:59:17 -0400

    $1

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -34,10 +34,12 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_material'
+#html_theme = 'piccolo_theme'
+#html_theme = 'sphinx_book_theme'
 
 html_static_path = ['_static']
 
-html_css_files = [
-    'custom.css',
-]
+#html_css_files = [
+#   'custom.css',
+#]
