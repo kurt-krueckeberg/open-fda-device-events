65fc296a4689c581e28a257166a6e95ab2c506a0
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-07-10 17:55:36 -0400

    $1

--- new-src/OpenFda.php
+++ new-src/OpenFda.php
@@ -5,6 +5,25 @@ namespace Fda;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
 
+/*
+ Query options:
+search: What to search for, in which fields. If you don’t specify a field to search, the API will search in every field.
+sort: Sort the results of the search by the specified field in ascending or descending order by using the :asc or :desc modifier.
+count: Count the number of unique values of a certain field, for all the records that matched the search parameter. By default, the API returns the 1000 most frequent values.
+limit: Return up to this number of records that match the search parameter. Currently, the largest allowed value for the limit parameter is 1000.
+skip: Skip this number of records that match the search parameter, then return the matching records that follow. Use in combination with limit to paginate results. Currently, the largest allowed value for the skip parameter is 25000. See Paging if you require paging through larger result sets.
+
+Are limit and skip pagenation, a type of iteration??
+
+meta.result.total gives the total hits, total number of records matching the search critieria
+
+class Results {
+
+    __ctor
+
+}
+ */
+
 class OpenFda extends RestApi implements OpenFdaInterface {
 
    private string $route;      
